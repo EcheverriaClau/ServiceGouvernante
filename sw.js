5b0fe7507f1a3836c8b978c26aa6aae66e1a1d7b
EcheverriaClau <echeverria.cla@gmail.com>
2026-04-14 22:49:08 -0400

    Change cache name to menage-hotel-v2
    Updated cache name and asset list for service worker.

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
 // ===== SERVICE WORKER =====
-const CACHE_NAME = 'menage-hotel-v1';
+const CACHE_NAME = 'menage-hotel-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/data.js',
-  '/js/app.js',
-  '/js/pdf.js',
-  '/manifest.json',
-  '/assets/icon-192.png',
-  '/assets/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;1,400&family=DM+Sans:wght@300;400;500&display=swap',
+  './',
+  './index.html',
+  './style.css',
+  './data.js',
+  './app.js',
+  './distribucion.js',
+  './horario.js',
+  './pdf.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+
 ];
 
 self.addEventListener('install', e => {
